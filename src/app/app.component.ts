a1baf714e5f9c3ca3d945ab7932e29a47f5bf0be
jobel johny <jobeljohny@gmail.com>
2023-11-12 19:04:25 +0530

    added navigation prevention when palettes present

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { KeyBindingService } from './services/keybinding.service';
+import { FoodPaletteService } from './services/food-palette.service';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,17 @@ import { KeyBindingService } from './services/keybinding.service';
 export class AppComponent {
   title = 'split-me';
 
-  constructor(private keyBinding: KeyBindingService) { }
+  constructor(
+    private keyBinding: KeyBindingService,
+    private foodPaletteService: FoodPaletteService
+  ) {}
 
   @HostListener('window:keydown', ['$event'])
   onKeyDown(event: KeyboardEvent): void {
     this.keyBinding.keydown(event);
   }
-
+  @HostListener('window:beforeunload', ['$event'])
+  canDeactivate(): boolean {
+    return this.foodPaletteService.palettes.length == 0;
+  }
 }

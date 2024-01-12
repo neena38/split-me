3115b5a4b76f798b15dffbe3dec75652946254c1
jobeljohny <jobeljohny@gmail.com>
2024-01-12 17:55:10 +0530

    about us with fixes

--- src/app/components/Help-Page/about-page/about-page.component.ts
+++ src/app/components/Help-Page/about-page/about-page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.scss']
+  styleUrls: ['./about-page.component.scss'],
 })
 export class AboutPageComponent {
-
+  @Output('onHelp') onHelp = new EventEmitter();
+  goToHelpPage() {
+    this.onHelp.emit();
+  }
 }

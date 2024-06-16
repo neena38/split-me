58442486692abd50d096fd234fb2da57092d57c1
jobeljohny <jobeljohny@gmail.com>
2024-06-16 12:35:32 +0530

    some changes

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { HelpDialogComponent } from 'src/app/help-dialog/help-dialog/help-dialog.component';
+import { pages } from 'src/app/help-dialog/help-dialog/help-page-utils';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent {
+  constructor(private dialog: MatDialog, private router: Router) {}
 
+  getStarted() {
+    this.router.navigate(['/app']);
+    this.dialog.open(HelpDialogComponent, {
+      width: '1200px',
+      height: '600px',
+      data: { page: pages.Help },
+    });
+  }
 }
